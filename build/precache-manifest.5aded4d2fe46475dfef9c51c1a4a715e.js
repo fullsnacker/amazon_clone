0d4e9aa4dbe27663a52ad24c5067ec70fb599f7e
fullsnacker <fullsnacker@gmail.com>
2022-07-21 20:00:59 -0300

    Deployinh content

---
+++ build/precache-manifest.5aded4d2fe46475dfef9c51c1a4a715e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8dd235d85852da2e7808e366c78f1cb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1d6d3879479ed97a8a69",
+    "url": "/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "3cfaf60270e7e0935af9",
+    "url": "/static/css/main.4912b8a7.chunk.css"
+  },
+  {
+    "revision": "1d6d3879479ed97a8a69",
+    "url": "/static/js/2.515c2bd6.chunk.js"
+  },
+  {
+    "revision": "3ceb8c17f560e6682fd3523301e674b3",
+    "url": "/static/js/2.515c2bd6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3cfaf60270e7e0935af9",
+    "url": "/static/js/main.1531f49f.chunk.js"
+  },
+  {
+    "revision": "92a146e5ab9e0f9e785b",
+    "url": "/static/js/runtime-main.90140090.js"
+  }
+]);
